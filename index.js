2ee209458e0695c012520917aa18bbc55495fab8
iamkoto <134742316+iamkoto@users.noreply.github.com>
2023-11-14 19:44:03 -0300

    bar lateral

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const bar = document.getElementById('header-menu__products-text');
 const overlay = document.getElementById('overlay');
 const menu = document.querySelector('.menu');
 const close = document.querySelector('.menu-title__box');
+const mobileBar = document.getElementById('header-menu__bar')
 
 bar.addEventListener('click', function() {
     overlay.style.display = 'block';
@@ -11,5 +12,10 @@ bar.addEventListener('click', function() {
 close.addEventListener('click', function(){
     overlay.style.display = 'none';
     menu.classList.remove('active');
+})
+
+mobileBar.addEventListener('click', function(){
+    overlay.style.display = 'block';
+    menu.classList.add('active');
 })
     

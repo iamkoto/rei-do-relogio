8707e27c54a11f3f284b41fa0b0d6ac205d408b2
iamkoto <134742316+iamkoto@users.noreply.github.com>
2023-11-16 18:03:45 -0300

    submenu

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const bar = document.getElementById('header-menu__products-text');
 const overlay = document.getElementById('overlay');
 const menu = document.querySelector('.menu');
+const submenu = document.querySelector('.submenu')
 const close = document.querySelector('.menu-title__box');
-const mobileBar = document.getElementById('header-menu__bar')
+const mobileBar = document.getElementById('header-menu__bar');
+const opensubmenu = document.querySelector('.menu-item');
+const closesubmenu = document.querySelector('.submenu-title__box');
+const backtomenu = document.querySelector('.submenu-top');
 
 bar.addEventListener('click', function() {
     overlay.style.display = 'block';
@@ -18,4 +22,22 @@ mobileBar.addEventListener('click', function(){
     overlay.style.display = 'block';
     menu.classList.add('active');
 })
+
+opensubmenu.addEventListener('click', function() {
+    submenu.classList.add('active');
+})
+
+closesubmenu.addEventListener('click', function() {
+    overlay.style.display = 'none';
+    submenu.classList.remove('active');
+    menu.classList.remove('active');
+})
+
+backtomenu.addEventListener('click', function() {
+    submenu.classList.remove('active');
+})
+
+
+
+
     
